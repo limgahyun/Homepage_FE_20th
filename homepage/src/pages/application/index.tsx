71c8fcad1996efe3111701bc91d040555a04e88a
lim-so-hyun <76216512+lim-so-hyun@users.noreply.github.com>
2024-01-21 19:40:04 +0900

    Design: 지원하기 height 조정

--- homepage/src/pages/application/index.tsx
+++ homepage/src/pages/application/index.tsx
@@ -5,7 +5,7 @@ import RepNotice from "./rep_notice";
 
 export default function Recruit() {
     return (
-        <div className="s:h-[3600px] h-[3200px] flex items-center justify-center ">
+        <div className="m:h-[3200px] s:h-[3600px] x:h-[3100px] h-[3100px] flex items-center justify-center ">
             <Background>
             <div className="font-pretendard">
                 <RepNotice/>
